ff4a3b9cc307b20650afbe4ed3e8f00cdbac2ea8
Nico Suarez-Canton Trueba <nsuarez.canton@gmail.com>
2016-12-10 18:11:51 +0100

    profile images are uplodaded to DB

---
+++ Firechat/LoginController+handlers.swift
@@ -0,0 +1,94 @@
+//
+//  LoginController+handlers.swift
+//  Firechat
+//
+//  Created by Nicolas Suarez-Canton Trueba on 12/8/16.
+//  Copyright © 2016 Nicolas Suarez-Canton Trueba. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func handleSelectProfileImageView() {
+        let profileImagePicker = UIImagePickerController()
+        
+        profileImagePicker.delegate = self
+        profileImagePicker.allowsEditing = true
+        
+        present(profileImagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage{
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Canceled picker.")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func handleRegister() {
+        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+            print("Form is not valid.")
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {
+            (user: FIRUser?, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            guard let uid = user?.uid else {
+                return
+            }
+            
+            // User succesfully authenticated.
+            let imageView = NSUUID().uuidString
+            let storageRef = FIRStorage.storage().reference().child("profile_images").child("\(imageView).png")
+            
+            if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!) {
+                storageRef.put(uploadData, metadata: nil, completion: { (metadata, error) in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    if let profileImageUrl = metadata?.downloadURL()?.absoluteString{
+                        let values = ["name": name, "email": email, "profileImageUrl": profileImageUrl]
+                        self.registerUserIntoDatabaseWithUID(uid: uid, values: values as [String : AnyObject])
+                    }
+                })
+            }
+        })
+    }
+    
+    private func registerUserIntoDatabaseWithUID(uid: String, values: [String: AnyObject]) {
+        let ref = FIRDatabase.database().reference(fromURL: "https://firechat-7ffee.firebaseio.com/")
+        let usersReference = ref.child("users").child(uid)
+        usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            if err != nil {
+                print(err!)
+                return
+            }
+            
+            // User succesfully added to DB
+            self.dismiss(animated: true, completion: nil)
+        })
+    }
+}
--- Firechat/LoginController.swift
+++ Firechat/LoginController.swift
@@ -60,40 +60,6 @@ class LoginController: UIViewController {
         }
     }
     
-    func handleRegister() {
-        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
-            print("Form is not valid.")
-            return
-        }
-        
-        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {
-            (user: FIRUser?, error) in
-            if error != nil {
-                print(error!)
-                return
-            }
-            
-            guard let uid = user?.uid else {
-                return
-            }
-            
-            // User succesfully authenticated.
-            
-            let ref = FIRDatabase.database().reference(fromURL: "https://firechat-7ffee.firebaseio.com/")
-            let usersReference = ref.child("users").child(uid)
-            let values = ["name": name, "email": email]
-            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-                if err != nil {
-                    print(err!)
-                    return
-                }
-                
-                // User succesfully added to DB
-                self.dismiss(animated: true, completion: nil)
-            })
-        })
-    }
-    
     let nameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Name"
@@ -130,11 +96,17 @@ class LoginController: UIViewController {
         return textField
     }()
     
-    let profileImageView: UIImageView = {
+    lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "firebase_logo")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
+        imageView.isUserInteractionEnabled = true
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView))
+        
+        imageView.addGestureRecognizer(tapRecognizer)
+        
         return imageView
     }()
     
